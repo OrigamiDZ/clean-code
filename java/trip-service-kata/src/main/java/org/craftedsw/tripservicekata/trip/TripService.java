3d0bf8bcad2d9cee08087a3c2229449bcfdbc91a
aurelia.del.zotto <aurelia96@free.fr>
2020-03-31 16:34:39 +0200

    Step Down Rule

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,37 +9,40 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	TripService(UserSession userSession, TripDAO tripDAO) {
-		this.userSession = userSession;
-		this.tripDAO = tripDAO;
-	}
+    TripService(UserSession userSession, TripDAO tripDAO) {
+        this.userSession = userSession;
+        this.tripDAO = tripDAO;
+    }
 
-	private UserSession userSession;
-	private TripDAO tripDAO;
+    private UserSession userSession;
+    private TripDAO tripDAO;
 
-	protected User getLoggedUser() {
-		return userSession.getLoggedUser();
-	}
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        User loggedUser = getLoggedUser();
+        checkLoggedUser(loggedUser);
+        return getTrips(user, loggedUser);
+    }
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		if (loggedUser != null) {
-			if (user.isFriend(loggedUser)) {
-				tripList = getFriendsTrips(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    private List<Trip> getTrips(User user, User loggedUser) {
+        if (user.isFriend(loggedUser)) {
+            return getFriendsTrips(user);
+        } else {
+            return new ArrayList<Trip>();
+        }
+    }
 
-	private List<Trip> getFriendsTrips(User user) {
-		List<Trip> tripList;
-		tripList = tripDAO.findTripsByUser(user);
-		return tripList;
-	}
+    private User getLoggedUser() {
+        return userSession.getLoggedUser();
+    }
 
+    private void checkLoggedUser(User loggedUser) throws UserNotLoggedInException {
+        if (loggedUser == null) {
+            throw new UserNotLoggedInException();
+        }
+    }
 
+    private List<Trip> getFriendsTrips(User user) {
+        return tripDAO.findTripsByUser(user);
+    }
 
 }
